9c052a23c6c9e9d2d6b820572ca49ecad957b15c
Achmad soewardi <soewardiachmad40@gmail.com>
2025-06-07 22:22:45 +0700

    menambahkan apps

--- src/main/java/com/mycompany/tindaklanjutku/Koneksi.java
+++ src/main/java/com/mycompany/tindaklanjutku/Koneksi.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package com.mycompany.tindaklanjutku;
 
 import java.sql.Connection;
@@ -9,38 +5,28 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author shaeh
- */
 public class Koneksi {
-
-    /**
-     * @param args the command line arguments
-     */
-    
     public static Connection konfig;
     
-    public static Connection configDB() throws SQLException{
+    public static Connection configDB() throws SQLException {
         try {
-//        inisialisasi variable untuk nama db
-        String url = "jdbc:mysql://localhost/tindaklanjutku";
-        
-//        Driver        
-        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-        
-//        Membuat Koneksi
-        konfig = DriverManager.getConnection(url, "root", "");
-        
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, e);
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost/tindaklanjutku?useSSL=false&serverTimezone=UTC";
+            konfig = DriverManager.getConnection(url, "root", "");
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Error: MySQL Driver tidak ditemukan!\nPastikan file mysql-connector-java.jar ada di classpath.");
+            System.exit(1);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error Koneksi Database:\n" + e.getMessage());
         }
         return konfig;
     }
     
     public static void main(String[] args) throws Exception {
-        // TODO code application logic here
-        Connection penghubung = (Connection)Koneksi.configDB();
+        Connection penghubung = configDB();
+        if (penghubung != null) {
+            JOptionPane.showMessageDialog(null, "Koneksi berhasil!");
+            penghubung.close();
+        }
     }
-    
 }
